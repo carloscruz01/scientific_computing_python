a1e0d30a7c257612101dfb0a097207eec448da74
carloscruz01 <contato.carloscruz29@gmail.com>
2022-11-29 22:05:47 -0300

    Uso de funções, com o intuito de armazenar uma estrutura definida de repetição, na qual faz-me entender a funcionalidade de alocação no valor i

---
+++ Anotações/interations_loops_definidos.py
@@ -0,0 +1,7 @@
+def linhas():
+    for i in [2,1,5]:
+        print(i)
+def box():
+    for o in ['Faxthe', 'Bastroff', 'goheut']:
+        print(o)
+box()
--- Anotações/repeated_steps.py
+++ Anotações/repeated_steps.py
@@ -6,11 +6,10 @@ def loop():
     print('Faxtoch')
     print(n) 
 n = 150
-while n > 0:
-    print(n)
-    n = n - 1
-    if n == 1:
-        break
-print('Faxtoch')
-
-
+def loop():
+    while n > 0:
+        print(n)
+        n = n - 1
+        if n == 1:
+            break
+    print('Faxtoch')
