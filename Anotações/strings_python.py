2a9a7ce3917b06a47100fc4ba115e4e4403abd03
carloscruz01 <contato.carloscruz29@gmail.com>
2022-11-30 21:56:10 -0300

    Loop e iterations para contar o a quantidade de vogais(a) na palavra banana.

---
+++ Anotações/strings_python.py
@@ -0,0 +1,9 @@
+print('hello world:')
+def numbera():
+    palavra = 'banana'
+    contagem = 0
+    for i in palavra:
+        if i == 'a':
+            contagem = contagem + 1
+    print(contagem)
+numbera()
