045095934640926e174ac5a8ef3773d158ee10ff
carloscruz01 <contato.carloscruz29@gmail.com>
2022-12-02 21:13:39 -0300

    Criação de um algoritmo, que busca criar um arquivo via comando Python e logo em seguida abri-lo.

--- Exercícios/exercice6.py
+++ Exercícios/exercice6.py
@@ -1,5 +1,14 @@
 def criation_file():
     open(mode="x", file='texto')
     open("texto")
-var = open("texto", "r", closefd=True)
-print(var.read())
+def abertura_arquivo():
+    var = open("texto", "r", closefd=True)
+    print(var.read())
+def abertura_arquivo2():
+    def criation_file():
+        open(mode="x", file="Text2")
+    text2 = open("text2", "r")
+    print(text2.read())
+    #criation_file()
+abertura_arquivo2()
+    
