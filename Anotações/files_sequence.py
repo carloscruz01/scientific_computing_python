b6dbf36d5609e438a2d4609e507163b7aa7c1928
carloscruz01 <contato.carloscruz29@gmail.com>
2022-12-03 11:16:12 -0300

    Criação de um arquivo através de comandos, no python. Uso do def() e diversos parâmetros para criação de um arquivo.

---
+++ Anotações/files_sequence.py
@@ -0,0 +1,12 @@
+def criation_file():    
+        test_read_file = open(mode="x", file="test_read_file")
+        open("test_read_file")
+    #criation_file()    
+def file342():
+    def open_file():
+        test_read_file = open("test_read_file", "r")
+        print(test_read_file.read())
+    open_file()
+file342()
+    
+    
